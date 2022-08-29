a643d2b68b2683a9df406e6068e522ad6c83a84b
Tommy Thorn <tommy-github2@thorn.ws>
2022-08-29 11:42:07 -0700

    Try a little PDP-8 inspired 4-bit CPU

--- src/user_module_341193419111006803.v
+++ src/user_module_341193419111006803.v
@@ -1,67 +1,220 @@
 `default_nettype none
 
+// Instructions
+`define Load 0
+`define Store 1
+`define Add 2
+`define Bnz 3
+
+// Commands
+`define Reset 0
+`define LoadCode 1
+`define LoadData 2
+`define Run 3
+
 // Keep I/O fixed for TinyTapeout
 module user_module_341193419111006803(
   input [7:0] io_in,
   output [7:0] io_out
 );
 
-   always @(*)
-     case (io_in[5:0])
-       0: io_out = 2;
-       1: io_out = 3;
-       2: io_out = 5;
-       3: io_out = 7;
-       4: io_out = 11;
-       5: io_out = 13;
-       6: io_out = 17;
-       7: io_out = 19;
-       8: io_out = 23;
-       9: io_out = 29;
-       10: io_out = 31;
-       11: io_out = 37;
-       12: io_out = 41;
-       13: io_out = 43;
-       14: io_out = 47;
-       15: io_out = 53;
-       16: io_out = 59;
-       17: io_out = 61;
-       18: io_out = 67;
-       19: io_out = 71;
-       20: io_out = 73;
-       21: io_out = 79;
-       22: io_out = 83;
-       23: io_out = 89;
-       24: io_out = 97;
-       25: io_out = 101;
-       26: io_out = 103;
-       27: io_out = 107;
-       28: io_out = 109;
-       29: io_out = 113;
-       30: io_out = 127;
-       31: io_out = 131;
-       32: io_out = 137;
-       33: io_out = 139;
-       34: io_out = 149;
-       35: io_out = 151;
-       36: io_out = 157;
-       37: io_out = 163;
-       38: io_out = 167;
-       39: io_out = 173;
-       40: io_out = 179;
-       41: io_out = 181;
-       42: io_out = 191;
-       43: io_out = 193;
-       44: io_out = 197;
-       45: io_out = 199;
-       46: io_out = 211;
-       47: io_out = 223;
-       48: io_out = 227;
-       49: io_out = 229;
-       50: io_out = 233;
-       51: io_out = 239;
-       52: io_out = 241;
-       53: io_out = 251;
-       default: io_out = 0;
-     endcase
+   // State
+   reg [1:0]   code[15:0];
+   reg [3:0]   data[15:0];
+   reg [3:0]   pc;
+   reg [3:0]   acc;
+
+   assign io_out = {pc, acc};
+   wire [3:0]  cmdarg = io_in[7:4];
+   wire [3:0]  arg = data[pc];
+
+   always @(posedge io_in[0])
+      case (io_in[2:1])
+        `Reset: begin
+           $display("        Reset");
+           pc <= 0;
+           acc <= 0;
+        end
+        `LoadCode: begin
+           $display("        LoadCode %d: Insn %d", pc, cmdarg);
+           code[pc] <= cmdarg;
+           pc <= pc + 1;
+        end
+        `LoadData: begin
+           $display("        LoadData %d: Data %d", pc, cmdarg);
+           data[pc] <= cmdarg;
+           pc <= pc + 1;
+        end
+        `Run: begin
+           $display("        Running %d (insn %d, %d)", pc, code[pc], data[pc]);
+           case (code[pc])
+             `Load: begin acc = arg; pc = pc + 1; end
+             `Store: begin data[arg] = acc; pc = pc + 1; end
+             `Add: begin acc = acc + arg; pc = pc + 1; end
+             `Bnz: begin pc = acc == 0 ? pc + 1 : arg; end
+           endcase
+           end
+      endcase
 endmodule
+
+`ifdef TB
+module tb;
+   reg clock = 1;
+   always #5 clock = !clock;
+
+   //reg [7:0] io_in;
+   reg [1:0] cmd;
+   reg [3:0] cmdarg;
+
+   wire [7:0] io_out;
+   wire [7:0] io_in = {cmdarg, 1'b0, cmd, clock};
+   user_module_341193419111006803 pdp0001(io_in, io_out);
+
+   initial begin
+      $monitor("%05d  pc %d acc %d", $time, pdp0001.pc, pdp0001.acc);
+      cmd = `Reset;
+      @(negedge clock)
+      cmd = `LoadCode;
+
+      cmdarg = `Load;
+      @(negedge clock)
+      cmdarg = `Store;
+      @(negedge clock)
+
+      cmdarg = `Add;
+      @(negedge clock)
+      cmdarg = `Store;
+      @(negedge clock)
+
+      cmdarg = `Load;
+      @(negedge clock)
+      cmdarg = `Store;
+      @(negedge clock)
+
+      cmdarg = `Load;
+      @(negedge clock)
+      cmdarg = `Add;
+      @(negedge clock)
+      cmdarg = `Store;
+      @(negedge clock)
+      cmdarg = `Bnz;
+      @(negedge clock)
+
+      cmdarg = `Load;
+      @(negedge clock)
+      cmdarg = `Bnz;
+      @(negedge clock)
+      cmdarg = `Bnz;
+      @(negedge clock)
+      cmdarg = `Bnz;
+      @(negedge clock)
+      cmdarg = `Bnz;
+      @(negedge clock)
+      cmdarg = `Bnz;
+      @(negedge clock)
+
+      cmd = `LoadData;
+
+      cmdarg = 1;
+      @(negedge clock)
+      cmdarg = 4;
+      @(negedge clock)
+      cmdarg = 1;
+      @(negedge clock)
+      cmdarg = 0;
+      @(negedge clock)
+      cmdarg = 9;
+      @(negedge clock)
+      cmdarg = 2;
+      @(negedge clock)
+      cmdarg = 5;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock)
+      cmdarg = 6;
+      @(negedge clock)
+      cmdarg = 0;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock)
+      cmdarg = 15;
+      @(negedge clock) cmd = `Reset;
+
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+      @(negedge clock) cmd = `Run;
+
+      #100 $display("The End");
+      $finish;
+   end
+endmodule
+`endif
